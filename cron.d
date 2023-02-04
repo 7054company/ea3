00b0ac36169e00fc27218cc28118cafd93ec1381
7054company <34761088+7054company@users.noreply.github.com>
2023-02-04 14:27:54 +0530

    Create cron.d

---
+++ cron.d
@@ -0,0 +1,4 @@
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin                                                                        
+
+
+*/2 * * * * root /usr/bin/7ea cron >/dev/null 2>&1
